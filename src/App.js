14bb8fa35c8fbd1642d04d33f42a5f52e9a65cc7
Boris <btapiaf@hotmail.com>
2021-02-02 01:45:27 -0500

    color e idiomas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, { Suspense } from "react";
+import React, { Suspense ,useState} from "react";
 import { AppRouter } from "./routers/AppRouter";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-
+import { UserContext } from "../src/hooks/UseContext";
 export const App = () => {
+
+  const [color, setColor] = useState(true)
   return (
+    <UserContext.Provider value={{
+      color,
+      setColor
+  }}>
     <Suspense fallback="loading">
       <Provider store={store}>
         <AppRouter />
       </Provider>
     </Suspense>
+    </UserContext.Provider>
   );
 };
